ad01c022f400ceb3a93d5286490567ad6eeb359d
Zhijing Zeng <zhijing.zeng@outlook.com>
2023-05-05 15:01:47 -0500

    End of Section 19

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, Input, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ReplaySubject, takeUntil } from 'rxjs';
 import { Message } from 'src/app/_models/message';
 import { MessageService } from 'src/app/_services/message.service';
 
@@ -11,20 +12,41 @@ import { MessageService } from 'src/app/_services/message.service';
 })
 export class MemberMessagesComponent implements OnInit {
   @Input() username?: string;
-
-  @ViewChild('messageForm') messageForm?: NgForm
+  @ViewChild('scrollMe') scrollMe?: ElementRef;
+  @ViewChild('messageForm') messageForm?: NgForm;
+  @ViewChildren('messages') messages?: QueryList<any>;
   messageContent='';
+  loading=false;
+  private destroy$ = new ReplaySubject<boolean>(1);
+
   constructor(public messageService: MessageService) { }
 
   ngOnInit(): void {
 
   }
-
+  ngAfterViewInit() {
+    this.scrollToBottom();
+    if(!this.messages) return;
+    this.messages?.changes.pipe(takeUntil(this.destroy$)).subscribe(this.scrollToBottom);
+  }
+  
+  scrollToBottom = () => {
+    if(!this.scrollMe) return
+    try {
+      this.scrollMe.nativeElement.scrollTop = this.scrollMe.nativeElement.scrollHeight;
+    } catch (err) {}
+  }
+  
   sendMessage(){
     if(!this.username) return;
+    this.loading=true;
     this.messageService.SendMessage(this.username, this.messageContent).then(()=>{
       this.messageForm?.reset();
-    })
+    }).finally(()=>this.loading = false);//if it is completed or error we will come here
+  }
+  ngOnDestroy(){
+    this.destroy$.next(true);
+    this.destroy$.complete(); 
   }
 
 }
